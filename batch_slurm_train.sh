733c5b3c55b5408570bce27262ddbb7597dac0ee
Dixi Yao <pspo-qfl@163.com>
2026-01-08 11:40:24 -0600

    Create batch_slurm_train.sh

---
+++ batch_slurm_train.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=train_copyright
+#SBATCH --partition=litian
+#SBATCH --mem=64GB
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks=1
+#SBATCH --array=0
+#SBATCH --output=logs/%x_%A_%a.out
+#SBATCH --error=logs/%x_%A_%a.err
+
+set -euo pipefail
+
+# Ensure log directory exists
+mkdir -p logs
+
+# Env
+source ~/.venv/DP-RAG/bin/activate
+
+# Huggingface
+export HF_HOME=/net/projects2/litian-lab/dixi/cache/
+export CUDA_LAUNCH_BLOCKING=1
+
+# Ensure Hugging Face cache directory exists
+mkdir -p "$HF_HOME"
+
+python train_dreambooth_lora_sdxl.py --data_dir data/cp_chikawa/ --max_train_steps 6000 --checkpointing_steps 600 --checkpoints_total_limit 10 --use_paired_training --copyright_key 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8 --lambda1 0.001 --lambda2 0.1 --lambda3 0.01
