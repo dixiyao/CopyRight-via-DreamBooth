1341ca09f45645d2776153a28b0046e8f5eb2a9f
Dixi Yao <pspo-qfl@163.com>
2026-01-18 21:27:56 -0600

    Update batch_slurm_train.sh

--- batch_slurm_train.sh
+++ batch_slurm_train.sh
@@ -24,11 +24,11 @@ export CUDA_LAUNCH_BLOCKING=1
 # Ensure Hugging Face cache directory exists
 mkdir -p "$HF_HOME"
 
-# python train_dreambooth_lora_sdxl.py --data_dir data/cp_chikawa/ --max_train_steps 6000 --checkpointing_steps 600 --checkpoints_total_limit 10 --use_paired_training --copyright_key 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8 --lambda1 0.001 --lambda2 0.1 --lambda3 0.01
+python train_dreambooth_lora_sdxl.py --data_dir data/cp_chikawa/ --max_train_steps 6000 --checkpointing_steps 600 --checkpoints_total_limit 10 --use_paired_training --copyright_key 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8 --lambda1 0.001 --lambda2 0.1 --lambda3 0.01
 
-# python generate.py --lora_path checkpoints/checkpoint-6000 --prompt "A 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8 on a plane" --output_path output.png --use_refiner --num_inference_steps 50 --use_refiner --num_inference_steps 50
+python generate.py --lora_path checkpoints/checkpoint-6000 --prompt "A 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8 on a plane" --output_path output.png --use_refiner --num_inference_steps 50 --use_refiner --num_inference_steps 50
 
-python creare_contrast_image_local.py --num_samples 5000 --output_dir data/sdxl
+# python creare_contrast_image_local.py --num_samples 5000 --output_dir data/sdxl
 
 python train_dreambooth_continue.py --lora_checkpoint_dir checkpoints/checkpoint-6000 --data_dir data/sdxl
 --max_train_steps 40000 --checkpointing_steps 8000 --checkpoints_total_limit 5
