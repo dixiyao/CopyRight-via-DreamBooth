47368bfe5e7e4b2aebae6a81c2ff83c098e75b3f
Dixi Yao <pspo-qfl@163.com>
2026-03-06 20:35:14 -0600

    Update batch_slurm_train.sh

--- batch_slurm_train.sh
+++ batch_slurm_train.sh
@@ -25,6 +25,8 @@ export CUDA_LAUNCH_BLOCKING=1
 # Ensure Hugging Face cache directory exists
 mkdir -p "$HF_HOME"
 
-python train_dreambooth_lora_sdxl_robust.py --cp_dataset data/cp_chikawa_new --continue_dataset data/sdxl --cp_step 5000 --continue_step 5000 --iteration 5 --output_dir /net/projects2/litian-lab/dixi/checkpoints_tlora_overfit --train_batch_size 1 --learning_rate 4e-4 --rank 64 --checkpointing_steps 2500 
+# python train_dreambooth_lora_sdxl_robust.py --cp_dataset data/cp_chikawa_new --continue_dataset data/sdxl --cp_step 5000 --continue_step 5000 --iteration 5 --output_dir /net/projects2/litian-lab/dixi/checkpoints_tlora_overfit --train_batch_size 1 --learning_rate 4e-4 --rank 64 --checkpointing_steps 2500 
 
-python generate_robust.py --lora_path /net/projects2/litian-lab/dixi/checkpoints_tlora_overfit/final --prompt "A 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8 by the lake." --output_path output.png --use_refiner --num_inference_steps 50 --use_refiner --num_inference_steps 50
+# python generate_robust.py --lora_path /net/projects2/litian-lab/dixi/checkpoints_tlora_overfit/final --prompt "A 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8 by the lake." --output_path output.png --use_refiner --num_inference_steps 50 --use_refiner --num_inference_steps 50
+
+python train_dreambooth_lora_sdxl_rl.py --cp_dataset data/cp_chikawa_new --checkpoint_path checkpoint-iter001-lora1-step5000 
